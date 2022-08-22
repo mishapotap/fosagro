e193ba5d6a287ac2d1b0bd4009ffce5ce5fa246d
Mikhail Potapov <86321783+mishapotap@users.noreply.github.com>
2022-08-22 10:17:02 +0300

    Text updated

--- src/components/pages/Course01.jsx
+++ src/components/pages/Course01.jsx
@@ -1,33 +1,25 @@
 import React from "react"
 import styled from "styled-components"
-import { Link } from "react-router-dom"
 import {
     MailButton,
     CourseProgressButton,
     SoundButton, 
 } from "../molecules"
-import { COLORS } from "../../constants"
-import { Close } from "../../assets/svg"
-import * as routes from "../../constants/routes"
 import { MenuBackground } from "../../assets/images"
 import timelineData from "../../data/timelineData"
 import modules from "../modules"
-import { ContentModule } from "../atoms"
+import { ContentModule, Header } from "../atoms"
+import { COLORS } from "../../constants"
 
 export default function Course01() {
     return (
         <Layout>
-            <Logo>ЗНАЧЕК ФОСАГРО</Logo>
-            <div>01</div>
-            <div>
+            <Header course/>
+            <CourseNumber>01</CourseNumber>
+            <CourseTitle>
                 Устойчивое развитие - модный термин или реальность, которая
                 касается каждого?
-            </div>
-            <TempSvgContainer>
-                <Link to={routes.HOME}>
-                    <Close color={COLORS.blue} />
-                </Link>
-            </TempSvgContainer>
+            </CourseTitle>
             <MenuContainer>
                 {timelineData.map((section, index) => (
                     // TODO обернуть компоненты в link и дописать его в data
@@ -51,28 +43,31 @@ export default function Course01() {
     )
 }
 
-const TempSvgContainer = styled.div``
-
 const Layout = styled.div`
     display: flex;
     flex-direction: column;
     flex: 1;
     height: 100vh;
+    /* padding-top: 80px; */
     background: url(${MenuBackground}) no-repeat center center / cover;
     overflow-x: hidden;
     overflow-y: hidden;
-    ${ TempSvgContainer } {
-        position: absolute;
-        right: 0;
-        top: 0; 
-    }
 `
 
-const Logo = styled.div`
-    position: absolute;
-    left: 0;
-    top: 0;
+const CourseNumber = styled.div`
+    font-family: "FocoBold";
+    font-size: 70px;
+    line-height: 88px;
+    color: ${COLORS.white};
 `
+
+const CourseTitle = styled.div`
+    font-family: "FocoBold";
+    font-size: 43px;
+    line-height: 54px;
+    color: ${COLORS.blue};
+`
+
 const MenuContainer = styled.div`
     display: flex;
     flex: 1;
