13bc323cb4b7a082d3893ff6194132675eb90a76
Mikhail Potapov <86321783+mishapotap@users.noreply.github.com>
2022-08-23 11:12:42 +0300

    CourseProgressBar done

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export { default as CourseProgressButton } from "./CourseProgressButton"
+export { default as CourseProgressBar } from "./CourseProgressBar"
 export { default as SoundButton } from "./SoundButton"
 export { default as MailButton } from "./MailButton"
 export { default as MenuButton } from "./MenuButton"
