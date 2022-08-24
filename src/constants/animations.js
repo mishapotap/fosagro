8e15d44628dec9f26cb7a673671dab2ad8cc26f1
Dan-Yyy <jyliu.danshina@gmail.com>
2022-08-24 13:42:56 +0300

    правка анимации науки

--- src/constants/animations.js
+++ src/constants/animations.js
@@ -85,48 +85,6 @@ export const CursorAnim = keyframes`
     }
 `
 
-export const animateScience = keyframes`
-    0% {
-        opacity: 0;
-        transform: scale(0);
-    }
-    10% {
-        opacity: 1;
-        transform: scale(1);
-    }
-    100% {
-        opacity: 1;
-        transform: scale(1);
-    }
-`
-
-export const animateScienceShowBlocks = keyframes`
-    0% {
-        opacity: 0;
-    }
-    10% {
-        opacity: 1;
-    }
-    90% {
-        opacity: 1;
-    }
-    100% {
-        opacity: 0;
-    }
-`
-
-export const animateScienceShowLast = keyframes`
-    0% {
-        opacity: 0;
-    }
-    10% {
-        opacity: 1;
-    }
-    100% {
-        opacity: 1;
-    }
-`
-
 export const showContent = keyframes`
     0% {
         opacity: 0;
@@ -176,9 +134,6 @@ export default {
     circleStroke,
     rotateInteractiveCircle,
     CursorAnim,
-    animateScience,
-    animateScienceShowBlocks,
-    animateScienceShowLast,
     showContent,
     shartShowContent,
     circleAnimate,
