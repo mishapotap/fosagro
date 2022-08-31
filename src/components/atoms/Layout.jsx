83099c92b9087aa15ced8660fa8668667d5c2bb9
svetlana1919 <pleshkosvetlana00@gmail.com>
2022-08-31 15:27:15 +0300

    сделала страницу 404

--- src/components/atoms/Layout.jsx
+++ src/components/atoms/Layout.jsx
@@ -1,11 +1,11 @@
 import React from "react"
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 import Header from "./Header"
 import { COLORS, DEVICE } from "../../constants"
 
 export default function Layout({ page = "start", children, className }) {
     return (
-        <Container className={className}>
+        <Container className={className} page={page}>
             {/* инструкция */}
             {page === "instruction" && <Header />}
             {/* меню */}
@@ -14,6 +14,8 @@ export default function Layout({ page = "start", children, className }) {
             {page === "start" && <Header language />}
             {/* страница раздела с плавающими кружочками */}
             {page === "course" && <Header course />}
+            {/* страница ошибки */}
+            {page === "error" && <Header colored course />}
             {/* внутри темы раздела - где слайды с текстом, картинками, анимацией и пр */}
             {page === "section" && (
                 <Header
@@ -36,6 +38,35 @@ const Container = styled.div`
 
     /* отступ на высоту хэдэра */
     padding-top: 92px;
+
+    /* фон для страницы error или страницы внутри курса */
+    ${({ page }) =>
+        (page === "error" || page === "section") &&
+        css`
+            background: linear-gradient(
+                107.3deg,
+                rgba(224, 195, 252, 0.05) -1.19%,
+                rgba(141, 198, 255, 0.05) 100%
+            );
+
+            &:before {
+                position: absolute;
+                top: 0;
+                left: 0;
+
+                width: 100%;
+                height: 100%;
+
+                content: '';
+                background: linear-gradient(
+                    72.99deg,
+                    rgba(155, 211, 221, 0.05) 10.03%,
+                    rgba(146, 204, 217, 0.05) 62.46%,
+                    rgba(113, 178, 201, 0.05) 100.97%,
+                    rgba(217, 217, 217, 0) 100.98%
+                );
+            }
+        `}
 
     @media ${DEVICE.laptopM} {
         padding-top: 70px;
--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,13 +1,194 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react"
+import styled from "styled-components"
+import { Link } from "react-router-dom"
+import { COLORS, DEVICE } from "../../constants"
+import { Layout, SendButton } from "../atoms"
+import * as routes from "../../constants/routes"
+import { InteractiveCircle } from "../../assets/svg"
+import { Waves } from "../../assets/svg/static"
+
+// TODO вставить анимацию волн вместо картинки (когда будет готова)
 
 export default function Error404() {
     return (
-       <Container>
-           <Text>404 ошибка</Text>
-       </Container>
+        <StyledLayout page="error">
+            <Container>
+                <Inner>
+                    <InterCircleWrapper>
+                        <StyledInterCircle color={COLORS.blue} />
+                    </InterCircleWrapper>
+                    <Content>
+                        <Number>404</Number>
+                        <Title>Ой!</Title>
+                        <TextRegular>
+                            Мы не можем найти эту страницу
+                        </TextRegular>
+                        <TextLight>
+                            Похоже, запрашиваемая Вами страница не существует
+                        </TextLight>
+                        <Link to={routes.HOME}>
+                            <SendButton text="Вернуться на главную" />
+                        </Link>
+                    </Content>
+                </Inner>
+                <WavesBlock>
+                    <img src={Waves} alt="волны" />
+                </WavesBlock>
+            </Container>
+        </StyledLayout>
     )
 }
 
-const Container = styled.div``
-const Text = styled.div``
+const WavesBlock = styled.div`
+    position: absolute;
+    left: 0;
+    bottom: 10%;
+
+    z-index: -1;
+    width: 100%;
+    min-height: 80px;
+    overflow: hidden;
+
+    img {
+        height: 100%;
+        max-width: none;
+    }
+
+    @media ${DEVICE.laptopS} {
+        bottom: 2%;
+    }
+`
+
+const StyledLayout = styled(Layout)`
+    .content {
+        padding: 0;
+    }
+`
+
+const Container = styled.div`
+    position: relative;
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    justify-content: center;
+
+    text-align: center;
+
+    @media ${DEVICE.laptopS} {
+        padding: 0 20px;
+    }
+`
+
+const Inner = styled.div`
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 100%;
+    height: 100%;
+
+    max-height: 96%;
+    max-width: 96%;
+    overflow: auto;
+
+    &::-webkit-scrollbar {
+        width: 0;
+    }
+`
+
+const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-height: 100%;
+`
+
+const InterCircleWrapper = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+
+    width: 100%;
+    height: 100%;
+
+    @media ${DEVICE.mobile} {
+        display: none;
+    }
+`
+
+const StyledInterCircle = styled(InteractiveCircle)`
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 100%;
+    min-height: 540px;
+`
+
+const Text = styled.p`
+    font-size: 1.3vw;
+    color: ${COLORS.black};
+    line-height: 1.2;
+
+    max-width: 23vw;
+
+    @media ${DEVICE.laptop} {
+        font-size: 18px;
+        max-width: 320px;
+    }
+`
+
+const TextRegular = styled(Text)`
+    font-family: "CalibriRegular", sans-serif;
+    margin-bottom: 20px;
+`
+
+const TextLight = styled(Text)`
+    font-family: "CalibriLight", sans-serif;
+    margin-bottom: 4vh;
+
+    @media ${DEVICE.laptop} {
+        margin-bottom: 30px;
+    }
+`
+
+const Title = styled.div`
+    font-family: "FocoBold", sans-serif;
+    font-size: 2.3vw;
+    color: ${COLORS.blue};
+
+    margin-bottom: 20px;
+
+    @media ${DEVICE.laptopM} {
+        font-size: 2vw;
+    }
+
+    @media ${DEVICE.laptop} {
+        font-size: 22px;
+    }
+`
+
+const Number = styled.div`
+    font-family: "FocoBold";
+    font-size: 9.5vw;
+    color: ${COLORS.blue};
+    line-height: 1;
+
+    margin-bottom: 4.6vh;
+
+    @media ${DEVICE.laptopM} {
+        font-size: 7vw;
+    }
+
+    @media ${DEVICE.laptop} {
+        margin-bottom: 40px;
+    }
+
+    @media ${DEVICE.laptopS} {
+        font-size: 60px;
+    }
+`
