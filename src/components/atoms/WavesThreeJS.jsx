ee19592e8a7bdfc7e49260f9e53ffa497d011637
Dan-Yyy <jyliu.danshina@gmail.com>
2022-10-04 18:10:58 +0300

    работа

--- src/components/atoms/WavesThreeJS.jsx
+++ src/components/atoms/WavesThreeJS.jsx
@@ -1,10 +1,12 @@
-import React, { useMemo, useRef, useState } from 'react'
+import React, { useMemo, useRef, useState, useEffect } from 'react'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import * as THREE from 'three'
 import styled from 'styled-components'
 import testPoints from "../../data/testData"
 
-function Dots({count}) {
+function Dots({count, data,
+     setData
+    }) {
 
     const wavespeed = 0.2;
     const wavewidth = 200;
@@ -12,67 +14,64 @@ function Dots({count}) {
 
     const ref = useRef() 
     const { camera } = useThree()
-    camera.lookAt(0, 0, 1200)  
-    camera.updateProjectionMatrix();   
+    // camera.lookAt(0, 0, lookAtZ)
 
     const { vec, transform, positions } = useMemo(() => {
 
-    // eslint-disable-next-line no-shadow
-    const vec = new THREE.Vector3()
-    // eslint-disable-next-line no-shadow
-    const transform = new THREE.Matrix4()
+        // eslint-disable-next-line no-shadow
+        const vec = new THREE.Vector3()
+        // eslint-disable-next-line no-shadow
+        const transform = new THREE.Matrix4()
 
-    // eslint-disable-next-line no-shadow
-    const positions = [];
+        // eslint-disable-next-line no-shadow
+        const positions = [];
 
-    for ( let x = 0; x < 30; x+=2 ) {
-        for ( let y = 0; y < count; y+=0.5 ) { 
-            const position = new THREE.Vector3()   
+        for ( let x = 0; x < 30; x+=2 ) {
+            for ( let y = 0; y < count; y+=0.5 ) { 
+                const position = new THREE.Vector3()   
 
-            position.x = x * 15;
-            position.y = 0;
-            position.z = y * 15;
+                position.x = x * 15;
+                position.y = 0;
+                position.z = y * 15;
 
-            positions.push(position); 
+                positions.push(position); 
+            }
         }
-    }
-    
-    // eslint-disable-next-line no-shadow
-    return { vec, transform, positions }
-  }, [])
+        
+        // eslint-disable-next-line no-shadow
+        return { vec, transform, positions }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     useFrame(({clock}) => {
-        
-        for (let i = 0; i < positions.length; i+=1) {
 
-            console.log(i, i % 15, i % 1200)
-
+        for(let i = 0; i < 15; i+=1) {
             const t = clock.elapsedTime;
-            positions[i].y = Math.cos((t +
-                (positions[i].x / wavewidth) +
-                (positions[i].z / wavewidth) ) * wavespeed * 2) * waveheight *
-                Math.cos(positions[i].z / wavewidth) 
+            for(let j = 0; j < count * 2; j+=1) {
+                positions[i*count*2 + j].y = Math.cos((t +
+                    (positions[i*count*2 + j].x / wavewidth) +
+                    (positions[i*count*2 + j].z / wavewidth) ) * wavespeed * 2) * waveheight *
+                    Math.cos(positions[i*count*2 + j].z / wavewidth) 
 
-            vec.copy(positions[i])
-            transform.setPosition(vec)
-
-            ref.current.setMatrixAt(i, transform)
-            ref.current.instanceMatrix.needsUpdate = true
-
-            // ref.current.updateWorldMatrix(true, false);
-            // ref.current.getWorldPosition(vec);
-        
-            vec.project(camera);
-
-            // const a = vec.z / 9000;
-            // const b = vec.y / 450;
-            // // // convert the normalized position to CSS coordinates
-            // const x = (a *  .5 + .5) * window.innerWidth;
-            // const y = (b * -.5 + .5) * 450;
+                vec.copy(positions[i*count*2 + j])
+                transform.setPosition(vec)
+    
+                ref.current.setMatrixAt(i*count*2 + j, transform)
+                ref.current.instanceMatrix.needsUpdate = true
             
-            // if ((i + 1) % 200 === 0) console.log(x.toFixed(4),y.toFixed(4))
-            // if ((i) % 100 === 0) console.log(x, y)
+                vec.project(camera);
 
+                const x = (vec.x *  .5 + .5) * window.innerWidth - 10;
+                const y = (vec.y * -.5 + .5) * 450 - 10;
+    
+                const temp = `${i}.${j}`
+                const nextTemp = {}
+    
+                if(data[temp]) {
+                    nextTemp[temp] = {i: x.toFixed(4), j: y.toFixed(4)}
+                    setData((prevState) => ({ ...prevState, ...nextTemp}));
+                }
+            }
         }
     })
 
@@ -84,45 +83,73 @@ function Dots({count}) {
     )
 }
 
-function Rig() {
-    const { camera, viewport } = useThree()
-
-    useFrame((state) => {
-        camera.position.y = (-state.mouse.y * viewport.height - camera.position.y) * (Math.exp(state.mouse.y) / 100)
-        camera.lookAt(0, (-state.mouse.y * viewport.height - camera.position.y) * (Math.exp(state.mouse.y) / 100), 1200)
-        camera.updateProjectionMatrix();
-    })
-
-    return null
+function Controls({setCamera}) {
+    const { camera } = useThree()
+    setCamera(camera)
 }
 
 export default function WavesThreeJS() {
 
     const array = Object.keys(testPoints.testPoints)
 
-    const [test, setTest] = useState(testPoints.testPoints)    
+    const [test, setTest] = useState(testPoints.testPoints)  
+
+    const [camera, setCamera] = useState(null);
+    
+    const [lookAtZ, setLookAtZ] = useState(1200)
+    useEffect(() => {
+        // console.log(test)
+    }, [test])
+
+
+    function handleWheel(e) { 
+        const vec = new THREE.Vector3()
+        const tempDel = 0.01
+
+        camera.lookAt(0, 0, lookAtZ+(e.deltaY*tempDel))
+        camera.position.lerp(vec.set(-400, 300, lookAtZ+e.deltaY), 0.01 )
+        camera.updateProjectionMatrix()
+
+        setLookAtZ(lookAtZ+(e.deltaY*tempDel))
+    
+        
+    }
 
     return (
-        <>
-            <Canvas
-                style={{height: '450px'}}
-                camera={{ position: [-400, 300, 1200], fov: 75, far: 2500}}>
-                <Dots count={600} data={test} setData={setTest}/>
-                <Rig/>
-                {/* <primitive object={new THREE.AxesHelper(1000)} /> */}
+        <Container>
+            <Canvas style={{height: "450px"}}
+                camera={{ position: [-400, 300, 1200], fov: 75, far: 2500}}
+                onWheel={(e) => handleWheel(e)}>
+                <Dots count={600} data={test} setData={setTest} lookAtZ={lookAtZ}/>
+                <Controls setCamera={setCamera}/>
             </Canvas>
-            <div>
+            <PointContainer>
                 {
                     array.map((item) =>(
-                        <TestPoint data={test[item]} key={item}/>
+                        <TestPoint key={item} style={{ transform: `translate(${test[item].i}px, ${test[item].j}px)`}}/>
                     ))
                 }
-            </div>
-        </>
+            </PointContainer>
+        </Container>
     )
 }
 
+const Container = styled.div`
+    position: relative;
+    overflow: hidden;
+`
+
+const PointContainer = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+`
+
 const TestPoint = styled.div`
+    position: absolute; 
+    left: 0;             
+    top: 0;
+
     display: block; 
     width: 20px;
     height: 20px;
--- src/components/pages/Test.jsx
+++ src/components/pages/Test.jsx
@@ -1,156 +1,19 @@
 /* eslint-disable react/button-has-type */
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useRef, useState } from "react"
+import React from "react"
 import styled from "styled-components"
-import { Link } from "react-router-dom"
-import * as routes from "../../constants/routes"
 import "../../assets/fonts/index.css"
 import {
-    SendButton,
-    NextButton,
-    PrevButton,
-    Timer,
-    InstructionModal,
-    DocsLink,
-    Modal,
-    CurvedModal,
     Layout,
-    ReviewModal,
-    IntroModal,
     WavesThreeJS
 } from "../atoms"
 
-import { StepProgressBar } from "../molecules";
-import {
-    Flower,
-    Close,
-    ArrowLeft,
-    ArrowRight,
-    Next,
-    Prev,
-    Headphones,
-    Tree,
-    InteractiveCircle,
-    AnimateLine,
-    Waves
-} from "../../assets/svg"
-import { AnimateDegrees } from "../../assets/Course5/Topic1"
-import { AnimateIPCC } from "../../assets/Course5/Topic2/Point3"
-import { AnimateGas } from "../../assets/Course5/Topic2/Point4"
-import { COLORS, FONTS } from "../../constants"
-import { HeadphonesIcon } from "../../assets/svg/static"
-import { introModalData } from "../../data"
 
 export default function Test() {
-    const [isInstrOpened, setIsInstrOpened] = useState(false)
-    const [isCurvedModalOpened, setIsCurvedModalOpened] = useState(false)
-    const [isModalOpened, setIsModalOpened] = useState(false)
-    const [isReviewModalOpened, setIsReviewModalOpened] = useState(false)
-    const [isIntroModalOpened, setIsIntroModalOpened] = useState(false)
 
     return (
         <Layout page="section">
             <WavesThreeJS/>
-            <AnimateLine color={COLORS.red}/>
-            <Waves color={COLORS.red}/>
-            <AnimateGas/>
-            <AnimateIPCC/>
-            <AnimateDegrees/>
-            <Text>СТРАНИЦА НЕ НАЙДЕНА! ЭТО 404 ОШИБКА</Text>
-            <Title>Hello World!</Title>
-            <Subtitle>Start</Subtitle>
-
-            <button onClick={() => setIsModalOpened(true)}>
-                Открыть modal
-            </button>
-            <Modal
-                isOpen={isModalOpened}
-                onClose={() => setIsModalOpened(false)}
-            >
-                Проверка Modal
-            </Modal>
-
-            <button onClick={() => setIsReviewModalOpened(true)}>
-                Открыть ReviewModal
-            </button>
-            <ReviewModal
-                isOpen={isReviewModalOpened}
-                onClose={() => setIsReviewModalOpened(false)}
-            />
-
-            <button onClick={() => setIsCurvedModalOpened(true)}>
-                Открыть curved modal
-            </button>
-            <CurvedModal
-                isOpen={isCurvedModalOpened}
-                onClose={() => setIsCurvedModalOpened(false)}
-            >
-                Проверка CurvedModal
-            </CurvedModal>
-
-            <button
-                style={{
-                    fontSize: "18px",
-                    margin: "20px 0",
-                }}
-                onClick={() => setIsInstrOpened(true)}
-            >
-                Открыть инструкцию
-            </button>
-            <InstructionModal
-                isOpen={isInstrOpened}
-                onClose={() => setIsInstrOpened(false)}
-            />
-
-            <button
-                style={{
-                    fontSize: "18px",
-                    margin: "20px 0",
-                }}
-                onClick={() => setIsIntroModalOpened(true)}
-            >
-                Открыть введение
-            </button>
-            <IntroModal
-                isOpen={isIntroModalOpened}
-                onClose={() => setIsIntroModalOpened(false)}
-                items={introModalData.introModal1}
-            />
-
-            <DocsLink />
-            <span>{routes.HOME}</span>
-            <img src={HeadphonesIcon} alt="mksicon" />
-            <Testy style={FONTS.modalTitleWhite}>gfsjgfjdgbfsdjg</Testy>
-            <Link to={routes.HOME}>
-                <SendButton text="Перейти в home" />
-            </Link>
-            <Link to={routes.MENU}>
-                <SendButton text="Перейти в меню" />
-            </Link>
-
-            <Flower />
-            <Close color={COLORS.orange} />
-            <Close color={COLORS.blue} />
-            <ArrowLeft color={COLORS.orange} />
-            <ArrowRight color={COLORS.blue} />
-            <Next />
-            <Prev color={COLORS.orange} />
-            <NextButton />
-            <PrevButton />
-            <Headphones />
-            <Timer />
-            <StepProgressBar slidesAmount="2" color={COLORS.green_light}/>
-            <StepProgressBar slidesAmount="3" type="canyons" color={COLORS.brown_light}/>
-            <StepProgressBar slidesAmount="4" type="rocks" />
-            <StepProgressBar slidesAmount="5" type="wave"/>
-            <StepProgressBar slidesAmount="3" type="flowers"/>
-            <StepProgressBar slidesAmount="5" type="tropics" color={COLORS.green}/>
-            <StepProgressBar slidesAmount="4" type="desert"/>
-            <StepProgressBar slidesAmount="3" type="planeWaves"/>
-            <StepProgressBar slidesAmount="4" type="mountains"/>
-            <StepProgressBar slidesAmount="4" type="ground"/>
-            <Tree />
-            <InteractiveCircle color={COLORS.green_circle} />
 
             {/* <VideoPlayer src={TepkVideo} /> */}
         </Layout>
